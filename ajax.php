8d8c11c53d80549e9de8e83f0d3b5f203c24bee9
liber87 <bd87@bk.ru>
2021-12-25 20:32:54 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,10 +1,10 @@
 <?php
 	if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
 		include_once './diff.class.php';
-		if (isset($_POST['text1'])){			
-			
-			$diff = new Diff($_POST['text1'],$_POST['text2']);
-			echo $diff->getTable();			
+		if ((isset($_POST['text1'])) && (isset($_POST['text1']))){					
+			$diff = new Diff($_POST['text1'],$_POST['text2'],$_POST['compare']);
+			if ($_POST['compare']=='nr') echo $diff->getTable();
+			else echo $diff->getProposals();
 			exit();
 		}	
 	}
